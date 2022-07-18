d08378e0319e1cb6b3393809a385cc6588439de4
Natta Wang <xenogew@gmail.com>
2022-07-19 01:37:45 +0900

    implement crawling into each student page and prepare types for collecting data

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { promises as fsPromises, createWriteStream, access } from "fs";
 import { join } from "path";
-import playwright from "playwright";
+import playwright, { BrowserContext } from "playwright";
 import https from "https";
 import { IncomingMessage } from "http";
 
 const studentListUrl = "https://bluearchive.wiki/wiki/Characters";
+var studentList = [];
 
-const asyncWriteFile = async (filename: string, data: any) => {
+const writeFile = async (filename: string, data: any):Promise<string> => {
   try {
     await fsPromises.writeFile(join(__dirname, filename), data, { flag: "a" });
     const contents = await fsPromises.readFile(
@@ -20,6 +21,31 @@ const asyncWriteFile = async (filename: string, data: any) => {
   }
 };
 
+const scrapProfile = async (items:Array<StudentLink>) => {
+  const browser = await playwright.chromium.launch();
+  const page = await browser.newPage();
+
+  for (var item of items) {
+    const url = item.link;
+    await page.goto(url, {timeout: 60000});
+
+    const name = await page.$eval('h1#firstHeading', e => e.textContent);
+    const baseStar = await page.$eval('div.character-rarity', e => e.dataset.value);
+    const background = await page.$eval('article#English-1', (e) => {
+      let b = e.textContent ? e.textContent : Array.from(e.children).map(e1 => e1.textContent);
+      
+      return Array.isArray(b) ? b.join(`\n`) : b;
+    });
+    // const school = await page.$eval('xpath=//html//body//div[3]//div[3]//div[5]//div[1]//table[1]//tbody//tr[4]//td[1]//p//img', e => e.getAttribute('alt'));
+    studentList.push(name);
+    console.log(`${name} ${baseStar}⭐`);
+    console.log(background);
+    // console.log(school);
+  }
+
+  await browser.close();
+};
+
 (async () => {
   const browser = await playwright.chromium.launch();
   const context = await browser.newContext({ acceptDownloads: true });
@@ -29,7 +55,7 @@ const asyncWriteFile = async (filename: string, data: any) => {
 
   await page.goto(studentListUrl);
 
-  const hrefs = await page.$$eval("a:has(img)", (elements) =>
+  const hrefs:Array<StudentLink> = await page.$$eval("a:has(img)", (elements) =>
     elements
       .filter((element: HTMLAnchorElement) => {
         const parentsRegex = /^((?!\().)*$/;
@@ -67,9 +93,13 @@ const asyncWriteFile = async (filename: string, data: any) => {
     });
   });
 
-  asyncWriteFile("./example.txt", JSON.stringify(hrefs, null, 2));
+  writeFile("./example.json", JSON.stringify(hrefs, null, 2));
 
   console.log(`${hrefs.length} links found`);
 
   await browser.close();
+
+  await scrapProfile(hrefs);
+
+  console.log(`scrapped: ${studentList.length} students`);
 })();
